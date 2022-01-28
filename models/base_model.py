f05c832bd22c327bb402a23fae3c3ac7c26b7a66
Nikodimosendashaw <nikodimos1111endashaw@gmail.com>
2022-01-28 13:28:53 +0000

    models/

---
+++ models/base_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+'''
+Basemodel class
+'''
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    '''
+    base model
+    '''
+
+    def __init__(self, *args, **kwargs):
+        '''
+        init
+        '''
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+
+    def save(self):
+        '''
+        update datetime
+        '''
+        self.updated_at = datetime.today()
+
+    def to_dict(self):
+        '''
+        Changes class instance to dictionary
+        '''
+        dict_ = self.__dict__.copy()
+        dict_['created_at'] = self.created_at.isoformat()
+        dict_['updated_at'] = self.updated_at.isoformat()
+        dict_['__class__'] = self.__class__.__name__
+        return dict_
+
+    def __str__(self):
+        '''
+        return [<class name>] (<self.id>) <self.__dict__>
+        '''
+        return f"{[self.__class__.__name__]} {(self.id)} {self.__dict__}"
