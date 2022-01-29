aa95863b0c689241981e27c9c8256e92a7177816
Nikodimosendashaw <nikodimos1111endashaw@gmail.com>
2022-01-29 18:32:25 +0000

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
-"""Basemodel class"""
-
-import models
+"""BaseModel that defines all common attributes/methods for other classes:"""
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """ base model"""
+    """Simple Base Model class"""
     def __init__(self, *args, **kwargs):
-        """initializing BaseModel"""
+        """initialize imprtant instance attribute"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
 
     def save(self):
-        """update datetime"""
+        """Update the current datetime"""
         self.updated_at = datetime.today()
 
     def to_dict(self):
-        """Changes class instance to dictionary"""
-        dict_ = self.__dict__.copy()
-        dict_["__class__"] = self.__class__.__name__
-        dict_["updated_at"] = self.updated_at.isoformat()
-        dict_["created_at"] = self.created_at.isoformat()
-        return dict_
+        """Returns the dict format of an object"""
+        kvdict = self.__dict__
+        kvdict["__class__"] = type(self).__name__
+        kvdict["updated_at"] = self.updated_at.isoformat()
+        kvdict["created_at"] = self.created_at.isoformat()
+        return kvdict
 
     def __str__(self):
-        """return [<class name>] (<self.id>) <self.__dict__>"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        """Returns information about the class in human readable format"""
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
