0a0da4575a0aa0e48c8478b2571f30e8d3ab65e9
Nikodimosendashaw <nikodimos1111endashaw@gmail.com>
2022-01-29 13:32:05 +0000

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
-#!/usr/bin/Python3
-"""BaseModel that defines all common attributes/methods for other classes:"""
-
+#!/usr/bin/python3
+'''
+Basemodel class
+'''
 
 import models
 import uuid
@@ -8,39 +9,36 @@ from datetime import datetime
 
 
 class BaseModel:
-    """Simple Base Model class"""
+    '''
+    base model
+    '''
+
     def __init__(self, *args, **kwargs):
-        """initialize imprtant instance attribute"""
+        '''
+        init
+        '''
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
-        """dictionary representation of an instance (method to_dict())."""
-
-        if len(kwargs) != 0:
-            strft = "%Y-%m-%dT%H:%M:%S.%f"
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    if key in ["created_at", "updated_at"]:
-                        self.__dict__[key] = datetime.strptime(value, strft)
-                    else:
-                        self.__dict__[key] = value
-        else:
-            models.storage.new(self)
 
     def save(self):
-        """Update the current datetime"""
+        '''
+        update datetime
+        '''
         self.updated_at = datetime.today()
-        models.storage.save()
 
     def to_dict(self):
-        """Returns the dict format of an object"""
-        kvdict = self.__dict__.copy()
-        # strtf = "%Y-%m-%dT%H:%M:%S.%f"
-        kvdict["__class__"] = type(self).__name__
-        kvdict["updated_at"] = self.updated_at.isoformat()
-        kvdict["created_at"] = self.created_at.isoformat()
-        return kvdict
+        '''
+        Changes class instance to dictionary
+        '''
+        dict_['__class__'] = type(self).__name__
+        dict_ = self.__dict__.copy()
+        dict_['created_at'] = self.created_at.isoformat()
+        dict_['updated_at'] = self.updated_at.isoformat()
+        return dict_
 
     def __str__(self):
-        """Returns information about the class in human readable format"""
-        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+        '''
+        return [<class name>] (<self.id>) <self.__dict__>
+        '''
+        return f"{[type(self).__name__]} ({self.id}) {self.__dict__}"
