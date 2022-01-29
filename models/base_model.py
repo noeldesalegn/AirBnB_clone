3595971d393132cc15719f3b7492f4a117cce479
Nikodimosendashaw <nikodimos1111endashaw@gmail.com>
2022-01-29 18:24:30 +0000

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -9,6 +9,7 @@ from datetime import datetime
 class BaseModel:
     """ base model"""
     def __init__(self, *args, **kwargs):
+        """initializing BaseModel"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
