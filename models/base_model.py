cdfe30630f7da820904cdef825601ca8082b8727
Nikodimosendashaw <nikodimos1111endashaw@gmail.com>
2022-01-29 13:30:53 +0000

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python3
-'''
-Basemodel class
-'''
+#!/usr/bin/Python3
+"""BaseModel that defines all common attributes/methods for other classes:"""
+
 
 import models
 import uuid
@@ -9,36 +8,39 @@ from datetime import datetime
 
 
 class BaseModel:
-    '''
-    base model
-    '''
-
+    """Simple Base Model class"""
     def __init__(self, *args, **kwargs):
-        '''
-        init
-        '''
+        """initialize imprtant instance attribute"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+        """dictionary representation of an instance (method to_dict())."""
+
+        if len(kwargs) != 0:
+            strft = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        self.__dict__[key] = datetime.strptime(value, strft)
+                    else:
+                        self.__dict__[key] = value
+        else:
+            models.storage.new(self)
 
     def save(self):
-        '''
-        update datetime
-        '''
+        """Update the current datetime"""
         self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
-        '''
-        Changes class instance to dictionary
-        '''
-        dict_['__class__'] = type(self).__name__
-        dict_ = self.__dict__.copy()
-        dict_['created_at'] = self.created_at.isoformat()
-        dict_['updated_at'] = self.updated_at.isoformat()
-        return dict_
+        """Returns the dict format of an object"""
+        kvdict = self.__dict__.copy()
+        # strtf = "%Y-%m-%dT%H:%M:%S.%f"
+        kvdict["__class__"] = type(self).__name__
+        kvdict["updated_at"] = self.updated_at.isoformat()
+        kvdict["created_at"] = self.created_at.isoformat()
+        return kvdict
 
     def __str__(self):
-        '''
-        return [<class name>] (<self.id>) <self.__dict__>
-        '''
-        return f"{[type(self).__name__]} ({self.id}) {self.__dict__}"
+        """Returns information about the class in human readable format"""
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
