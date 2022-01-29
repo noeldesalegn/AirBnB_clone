6f566ff53b50839ff718b9e5e069236deef57ed3
Nikodimosendashaw <nikodimos1111endashaw@gmail.com>
2022-01-29 17:25:08 +0000

    test

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,10 @@ from datetime import datetime
 
 class BaseModel:
     """ base model"""
-      
     def __init__(self, *args, **kwargs):
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.today()
-            self.updated_at = datetime.today()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
 
     def save(self):
         """update datetime"""
