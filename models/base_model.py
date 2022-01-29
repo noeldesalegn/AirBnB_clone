568e1782c0efca25f610640dfe403c445f13b6ab
Nikodimosendashaw <nikodimos1111endashaw@gmail.com>
2022-01-29 14:05:58 +0000

    test

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel:
     base model
     '''
       
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
             self.id = str(uuid.uuid4())
             self.created_at = datetime.today()
             self.updated_at = datetime.today()
