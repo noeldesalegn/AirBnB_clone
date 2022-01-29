c9549a10fe95555259afad2cab76670498610568
Nikodimosendashaw <nikodimos1111endashaw@gmail.com>
2022-01-29 12:21:49 +0000

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -39,4 +39,4 @@ class BaseModel:
         '''
         return [<class name>] (<self.id>) <self.__dict__>
         '''
-        return f"{[self.__class__.__name__]} {(self.id)} {self.__dict__}"
+        return f"{[self.__class__.__name__]} ({self.id}) {self.__dict__}"
