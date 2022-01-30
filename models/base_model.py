07700d79144dbf75224ca26a11820518dfa6f41f
Nikodimosendashaw <nikodimos1111endashaw@gmail.com>
2022-01-30 05:33:48 +0000

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,47 +1,29 @@
 #!/usr/bin/python3
-from turtle import st
+"""BaseModel that defines all common attributes/methods for other classes:"""
 import uuid
 from datetime import datetime
-from models.__init__ import storage
-
-"""
-This is the module that contains the base class
-which defines all common attributes/methods
-for other classes.
-"""
 
 
-class BaseModel():
-    """ Init method """
+class BaseModel:
+    """Simple Base Model class"""
     def __init__(self, *args, **kwargs):
-        if len(kwargs) != 0:
-            for key in kwargs:
-                if key == 'id':
-                    self.id = kwargs.get(key)
-                if key == 'created_at':
-                    self.created_at = datetime.strptime(kwargs.get(key), '%Y-%m-%dT%H:%M:%S.%f')
-                if key == 'updated_at':
-                    self.updated_at = datetime.strptime(kwargs.get(key), '%Y-%m-%dT%H:%M:%S.%f')
-                if key == 'my_number':
-                    self.my_number = kwargs.get(key)
-                if key == 'name':
-                    self.name = kwargs.get(key)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+        """initialize imprtant instance attribute"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
 
     def save(self):
-        self.updated_at = datetime.utcnow()
-        storage.save()
+        """Update the current datetime"""
+        self.updated_at = datetime.today()
 
     def to_dict(self):
-        basedict = self.__dict__
-        basedict['__class__'] = self.__class__.__name__
-        basedict['created_at'] = self.created_at.isoformat("T")
-        basedict['updated_at'] = self.updated_at.isoformat("T")
-        return basedict
+        """Returns the dict format of an object"""
+        kvdict = self.__dict__
+        kvdict["__class__"] = type(self).__name__
+        kvdict["updated_at"] = self.updated_at.isoformat()
+        kvdict["created_at"] = self.created_at.isoformat()
+        return kvdict
 
     def __str__(self):
-        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+        """Returns information about the class in human readable format"""
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
