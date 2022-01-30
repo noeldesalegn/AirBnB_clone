bce5dea9ec796fac1fe379b9c8c36b0bae8f8ae8
Nikodimosendashaw <nikodimos1111endashaw@gmail.com>
2022-01-30 05:37:29 +0000

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,29 +1,58 @@
 #!/usr/bin/python3
-"""BaseModel that defines all common attributes/methods for other classes:"""
-import uuid
-from datetime import datetime
+from uuid import uuid4
+import datetime
+import models
+"""
+command interpreter to manage your AirBnB objects clone version
+"""
 
 
 class BaseModel:
-    """Simple Base Model class"""
+    """ This  defines all common attributes/methods for other classes
+    """
+    st = "%Y-%m-%dT%H:%M:%S.%f"
+    t = datetime.datetime
+
     def __init__(self, *args, **kwargs):
-        """initialize imprtant instance attribute"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-
-    def save(self):
-        """Update the current datetime"""
-        self.updated_at = datetime.today()
-
-    def to_dict(self):
-        """Returns the dict format of an object"""
-        kvdict = self.__dict__
-        kvdict["__class__"] = type(self).__name__
-        kvdict["updated_at"] = self.updated_at.isoformat()
-        kvdict["created_at"] = self.created_at.isoformat()
-        return kvdict
+        """ Public instance attributes:
+        """
+        if kwargs:
+            for key, val in kwargs.items():
+                if "created_at" == key:
+                    self.created_at = t.strptime(kwargs["created_at"], st)
+                elif "updated_at" == key:
+                    self.updated_at = t.strptime(kwargs["updated_at"], st)
+                elif "__class__" == key:
+                    pass
+                else:
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.datetime.today()
+            self.updated_at = datetime.datetime.today()
+            models.storage.new(self)
 
     def __str__(self):
-        """Returns information about the class in human readable format"""
-        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+        """ this magic fuction print valuse in a nice way  """
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """ updates the public instance attribute updated_at
+        with the current datetime
+        """
+        return("{}".format(self.created_at))
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/values of
+        __dict__ of the instance
+        """
+        keys = {}
+        keys["__class__"] = self.__class__.__name__
+        for i, val in self.__dict__.items():
+            if isinstance(val, (datetime.datetime, )):
+                keys[i] = val.isoformat()
+            else:
+                keys[i] = val
+        return keys
