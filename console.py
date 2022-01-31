8e57c9dd062d28aacd283fb9445fb21b6a40cb16
Nikodimosendashaw <nikodimos1111endashaw@gmail.com>
2022-01-31 12:27:03 +0000

    console.py

--- console.py
+++ console.py
@@ -8,12 +8,17 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
+    def emptyline(self):
+        """pass"""
+        pass
+
     def do_EOF(self, line):
         """EOF to quit"""
         return True
 
     def do_quit(self, line):
-        """Quit command to exit the program"""
+        """Quit command to exit the program
+        """
         return True
 
 
