23105cb0cc976a2cbce2f6ee6d5d1de86f2eb694
Nikodimosendashaw <nikodimos1111endashaw@gmail.com>
2022-01-31 12:14:05 +0000

    console.py

---
+++ console.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Console.py"""
+
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb)'
+    def do_EOF(self, line):
+        """EOF to quit"""
+        return True
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
